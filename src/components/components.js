bc402e7311e7f9ba183f0e4fa8ece9160eed008d
Victor <victor.garcia@heg.com>
2017-04-30 10:11:09 +0100

    Creation of flight-list-empty-item componennt, implementation of this item on flight-list component

--- src/components/components.js
+++ src/components/components.js
@@ -5,6 +5,7 @@ import ButtonComponent from './atoms/button/button.component';
 
 import AirportSelectorComponent from './molecules/airport-selector/airport-selector.component';
 import FlightListItem from './molecules/flight-list-item/flight-list-item.component';
+import FlightListEmptyItem from './molecules/flight-list-empty-item/flight-list-empty-item.component';
 
 import FlightListComponent from './organisms/flight-list/flight-list.component';
 import CheapFlightsManagerComponent from './organisms/cheap-flights-manager/cheap-flights-manager.component';
@@ -15,6 +16,7 @@ export default angular.module('app.components', [])
 
 .component('airportSelector', AirportSelectorComponent)
 .component('flightListItem', FlightListItem)
+.component('flightListEmptyItem', FlightListEmptyItem)
 
 .component('flightList', FlightListComponent)
 .component('cheapFlightsManager', CheapFlightsManagerComponent)
---
+++ src/components/molecules/flight-list-empty-item/flight-list-empty-item.component.js
@@ -0,0 +1,8 @@
+import template from './flight-list-empty-item.component.html';
+
+const FlightListEmptyItemComponent = {
+  template,
+};
+
+export default FlightListEmptyItemComponent;
+
