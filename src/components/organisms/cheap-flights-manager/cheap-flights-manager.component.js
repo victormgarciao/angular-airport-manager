453ec7eed94bd51323d990125971153d065e1ff0
Victor <victor.garcia@heg.com>
2017-05-01 14:14:51 +0100

    cheap-flights-manager styled, also loading button styled making the loader with the same color than the text

--- src/components/organisms/cheap-flights-manager/cheap-flights-manager.component.js
+++ src/components/organisms/cheap-flights-manager/cheap-flights-manager.component.js
@@ -1,5 +1,6 @@
 import template from './cheap-flights-manager.component.html';
 import controller from './cheap-flights-manager.controller';
+import './cheap-flights-manager.component.scss';
 
 const CheapFlightsManagerComponent = {
   template,
